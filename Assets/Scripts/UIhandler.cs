1dd1542ae08c94831da0aca8cd6c5855a4208a76
prucinski <p.rucinski.19@abdn.ac.uk>
2022-01-17 16:24:10 +0000

    Added title screen, buttons and UI event handling class. Need a break for figuring out why my buttons aren't working

---
+++ Assets/Scripts/UIhandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//frankly speaking it has been a while since I worked on UI, so I am making it relatively basic and code-based.
+public class UIhandler : MonoBehaviour
+{
+    public GameObject contButton;
+    public GameObject titleText;
+    public GameObject signature;
+
+    public void clickedContinue()
+    {
+        Debug.Log("Continue button clicked");
+        contButton.SetActive(false);
+        titleText.SetActive(false);
+        signature.SetActive(false);
+    }
+
+}
